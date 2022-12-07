31c5b056c595b5251181a66bc33d363911a6493d
Nikola Dojcinovic <goonergalactico@gmail.com>
2022-12-07 14:59:10 +0100

    finished responsive

--- challengeonde/src/components/Container.jsx
+++ challengeonde/src/components/Container.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import mainImg from "../assets/image-product-desktop.jpg";
+import mobileImg from "../assets/image-product-mobile.jpg";
 import Button from "../constants/Button";
 
 const Container = () => {
   return (
-    <div className="wrapper bg-white rounded-xl shadow-lg flex">
+    <div className="wrapper bg-white rounded-xl shadow-lg flex flex-col sm:flex sm:flex-row">
       <div className="image-container">
         <img
-          className="w-[330px] h-[100%] object-cover rounded-l-xl"
+          className="hidden sm:flex w-[330px] h-[100%] object-cover rounded-l-xl"
           src={mainImg}
+          alt="main-image"
+        />
+        <img
+          className="sm:hidden w-[100%] h-[100%] object-cover rounded-t-xl block"
+          src={mobileImg}
           alt="main-image"
         />
       </div>
@@ -17,9 +23,15 @@ const Container = () => {
           Perfume
         </p>
 
-        <h1 className="text-4xl leading-10 my-5">
+        <h1 className="hidden sm:flex text-4xl leading-10 my-5">
           Gabrielle <br />
           Essence Eau <br />
+          De Parfum
+        </h1>
+
+        <h1 className="text-4xl leading-10 my-5 sm:hidden">
+          Gabrielle 
+          Essence Eau
           De Parfum
         </h1>
 
@@ -30,11 +42,11 @@ const Container = () => {
           for the House of CHANEL.
         </p>
 
-        <span className="price text-3xl text-darkCyan">$149.99</span>
+        <span className="price text-4xl sm:text-3xl text-darkCyan">$149.99</span>
         <span className="line-through text-darkGrayishBlue relative bottom-1 left-6">
           $169.99
         </span>
-        <Button/>
+        <Button />
       </div>
     </div>
   );
--- challengeonde/src/constants/Button.jsx
+++ challengeonde/src/constants/Button.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from "react";
 import { BiCart } from "react-icons/bi";
-
 
 const Button = () => {
   return (
-    <div className="flex items-center justify-center bg-darkCyan rounded-md text-white p-3 transition-all cursor-pointer mt-5 hover:bg-veryDarkBlue">
+    <div className="flex items-center justify-center bg-darkCyan rounded-md text-white p-4 transition-all cursor-pointer mt-5 hover:bg-veryDarkBlue sm:p-3">
       <BiCart className="w-[20px] h-[20px]"></BiCart>
-      <p className='mx-2'>Add to Cart</p>
+      <p className="mx-3 sm:mx-2 font-bold">Add to Cart</p>
     </div>
-  )
-}
+  );
+};
 
 export default Button;
